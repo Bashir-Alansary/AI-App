f603331f6de23e27d862b03b00eef9861fdb3188
Basher <bashiralansary306@gmail.com>
2026-05-21 18:54:44 +0300

    Added a new, empty AiChatController with basic routing and API controller setup.

---
+++ Controllers/AiChatController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AI_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AiChatController : ControllerBase
+    {
+    }
+}
